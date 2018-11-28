a359540057a6f65bfd86a401e2f9bb3e818edb80
Aiki Trumm <aikitrumm@Maarjas-MBP.lan>
2018-11-28 19:31:32 +0200

    Day2

---
+++ Day2_26112018/lp3thw_ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Aiki Trumm'
+my_age = 38 # not a lie
+my_height = 175 # sentimeetrit
+my_weight = 96 # kilogrammi
+my_eyes = 'sinised'
+my_teeth = 'valged'
+my_hair = 'pruunid'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} centimeters tall.")
+print(f"He's {my_weight} kg heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f" If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
