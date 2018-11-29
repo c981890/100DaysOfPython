8ea547f640fa0b9f0825ff7c8cec9bfbbb27142b
Aiki Trumm <aikitrumm@Maarjas-MBP.lan>
2018-11-29 16:29:51 +0200

    Day5

---
+++ Day5_29112018/ps0.py
@@ -0,0 +1,14 @@
+# Problem Set 0
+# Name: Aiki Trumm
+# Collaborators: None
+# Time Spent:
+
+# Write a program that does the following in order:
+# Asks the user to enter his/her date of birth.
+# Asks the user to enter his/her last name.
+# Prints out the user’s last name and date of birth, in that order.
+
+birthday = input("Enter your date of birth: ")
+name = input("Enter your last name: ")
+
+print(name, birthday)
