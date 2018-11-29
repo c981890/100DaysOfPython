8609f158c69e6e90a6cc9864d3e706464d75a645
Aiki Trumm <aikitrumm@Maarjas-MBP.lan>
2018-11-29 16:31:07 +0200

    Aeg

--- Day5_29112018/ps0.py
+++ Day5_29112018/ps0.py
@@ -1,7 +1,7 @@
 # Problem Set 0
 # Name: Aiki Trumm
 # Collaborators: None
-# Time Spent:
+# Time Spent: 0:15
 
 # Write a program that does the following in order:
 # Asks the user to enter his/her date of birth.
