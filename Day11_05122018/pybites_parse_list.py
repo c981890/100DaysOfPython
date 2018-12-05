a8373c6b0ba6a7c4c581d5fdeaf05f6b88a38886
Aiki Trumm <aikitrumm@Maarjas-MBP.lan>
2018-12-05 12:23:36 +0200

    Parandused

--- Day11_05122018/pybites_parse_list.py
+++ Day11_05122018/pybites_parse_list.py
@@ -49,4 +49,24 @@ def shortest_first_name(names):
     sorted_names = sorted(splitted_names, key=length_of_first_item)
     return sorted_names[0][0]
 
-shortest_first_name(NAMES)
+"""
+Alternative way
+def dedup_and_title_case_names(names):
+    """Should return a list of names, each name appears only once"""
+    return list({name.title() for name in names})
+
+
+def sort_by_surname_desc(names):
+    """Returns names list sorted desc by surname"""
+    names = dedup_and_title_case_names(names)
+    return sorted(names,
+                  key=lambda x: x.split()[-1],
+                  reverse=True)
+
+
+def shortest_first_name(names):
+    """Returns the shortest first name (str)"""
+    names = dedup_and_title_case_names(names)
+    names = [name.split()[0] for name in names]
+    return min(names, key=len)
+"""
