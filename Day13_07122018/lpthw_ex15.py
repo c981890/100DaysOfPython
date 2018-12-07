10d489de01e09b9cf9e74d94e7fba0d4d7309554
Aiki Trumm <aikitrumm@Maarjas-MBP.lan>
2018-12-07 10:11:12 +0200

    Day 13

---
+++ Day13_07122018/lpthw_ex15.py
@@ -0,0 +1,15 @@
+from sys import argv
+from string import Template
+
+script, filename = argv
+txt = open(filename)
+
+templ_string = "Here's your $file:"
+print(Template(templ_string).substitute(file=filename))
+
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+txt_again = open(file_again)
+print(txt_again.read())
