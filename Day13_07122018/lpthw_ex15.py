595fd52be008c4b43a52eebae0d0123693e4349c
Aiki Trumm <aikitrumm@Maarjas-MBP.lan>
2018-12-07 10:23:17 +0200

    close()

--- Day13_07122018/lpthw_ex15.py
+++ Day13_07122018/lpthw_ex15.py
@@ -8,8 +8,10 @@ templ_string = "Here's your $file:"
 print(Template(templ_string).substitute(file=filename))
 
 print(txt.read())
+txt.close()
 
 print("Type the filename again:")
 file_again = input("> ")
 txt_again = open(file_again)
 print(txt_again.read())
+txt.close()
