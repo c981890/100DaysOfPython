54d5effd5a33541a878a9c20f87f4adcb285dfbd
Aiki Trumm <aikitrumm@Maarjas-MBP.lan>
2018-12-19 17:37:04 +0200

    Day 21

---
+++ Day21_18122018/lpthw_ex40.py
@@ -0,0 +1,26 @@
+"""LPTHW Exercise 40."""
+
+
+class Song(object):
+    """Docstring."""
+
+    def __init__(self, lyrics):
+        """Docstring."""
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        """Docstring."""
+        for line in self.lyrics:
+            print(line)
+
+
+happy_bday = Song(["Happy birthday to you",
+                   "I don't want to get sued",
+                   "So I'll stop right there"])
+
+bulls_on_parade = Song(["They rally around the family",
+                        "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
