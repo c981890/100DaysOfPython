217ecded2ff575b98fd55866c2bdd30c297f69ba
Aiki Trumm <aikitrumm@Maarjas-MBP.lan>
2019-01-27 10:28:25 +0200

    Muudatused

---
+++ Day27_26012019/test3.py
@@ -0,0 +1,12 @@
+'''paaride_hulk = {(-2, 8), (3, 7), (2, 9)}
+tulemus = 0
+
+for paar in paaride_hulk.add((1, 4)):
+    tulemus += paar[0]
+print(tulemus)'''
+
+print({1, 4, 2} | {1, 2, 7} & {3, 1})
+
+print({9, 1, 7} ^ {1, 7, 9})
+
+print(len(list(set((1, 3, 6, 3, 1)))))
