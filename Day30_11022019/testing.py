027075dfd6f7dcedc0228e092bac93f4ed9b3019
Aiki Trumm <aikitrumm@Maarjas-MBP.lan>
2019-02-11 08:30:09 +0200

    Day 30

--- Day30_11022019/testing.py
+++ Day30_11022019/testing.py
@@ -7,3 +7,37 @@ def rek_fun(n):
 
 
 rek_fun(-7)
+
+
+def print_alla(n):
+    if n <= 0:
+        print("Stop!")
+    else:
+        print(n)
+        print_alla(n - 1)
+
+
+print_alla(4)
+
+
+def print_ules(n):
+    if n <= 0:
+        print("Stop!")
+    else:
+        print_ules(n-1)
+        print(n)
+
+
+print_ules(4)
+
+
+def print_alla_ules(n):
+    if n <= 0:
+        print("Stop!")
+    else:
+        print(n)
+        print_alla_ules(n - 1)
+        print(n)
+
+
+print_alla_ules(4)
