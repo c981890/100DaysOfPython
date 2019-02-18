be5f6beb19554ccb1924fd2118256e1c397cbbf3
Aiki Trumm <aiki.trumm@mail.ee>
2019-02-18 09:04:12 +0200

    commit

--- Day31_17022019/yl6.2draft.py
+++ Day31_17022019/yl6.2draft.py
@@ -5,7 +5,10 @@ def seosta_lapsed_ja_vanemad(lapsed, nimed):
         osad = rida.strip('\n').split(' ', 1)
         nimed_sonastik[osad[0]] = osad[1]
     fail.close()
+    # print("nimed" + "\n", nimed_sonastik)
+
     seostatud_sonastik = {}
+
     fail = open(lapsed, encoding="UTF-8")
     lapsed_sonastik = {}
     for rida in fail:
@@ -17,4 +20,9 @@ def seosta_lapsed_ja_vanemad(lapsed, nimed):
             seostatud_sonastik[vanem] = set()
         seostatud_sonastik[vanem].add(laps)
     fail.close()
+    # print("lapsed" + "\n", lapsed_sonastik)
+    # print("seostaud" + "\n", seostatud_sonastik)
     return seostatud_sonastik
+
+
+# seosta_lapsed_ja_vanemad('lapsed.txt', 'nimed.txt')
